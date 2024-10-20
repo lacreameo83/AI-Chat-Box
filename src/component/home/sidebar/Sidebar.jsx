f63c755eaf4b21c21ca2a59177c86ae34df17881
precious osonwa <142677806+lacreameo83@users.noreply.github.com>
2024-10-20 07:56:33 -0500

    did something

--- src/component/home/sidebar/Sidebar.jsx
+++ src/component/home/sidebar/Sidebar.jsx
@@ -68,9 +68,9 @@ function Sidebar() {
           <SideBarItems primary="Promo Page" />
 
           {/* previous value */}
-          <div className="pl-2 h-fit displayResult  overflow-y-scroll">
+          <div className="pl-2 h-[30vh] displayResult  overflow-y-scroll">
             {ispreviousValue ? (
-              <div className=" m-auto bg-stone-100">
+              <div className="  m-auto bg-stone-100">
                 <h2 className="font-[900] sticky top-0 bg-stone-50 h-[5vh]  text-center pt-2">
                   Previous Search
                 </h2>
@@ -129,7 +129,7 @@ function Sidebar() {
                   src={image}
                 />
               </div>
-             <p className=''>osonwa precious</p>
+              <p className="">osonwa precious</p>
               <IosShareIcon sx={{ marginLeft: "20px" }} />
             </div>
           </div>
